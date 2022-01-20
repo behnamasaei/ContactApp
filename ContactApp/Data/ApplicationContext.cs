1bdd705ee9ca5b7c523157d45565f0d875de3109
behnamasaei <behnamasaei@gmail.com>
2022-01-20 20:05:41 +0330

    Initialize Application Context

---
+++ ContactApp/Data/ApplicationContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactApp.Data;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<ContactData> Contacts { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source=C:\Users\behna\Desktop\ContactDB.db;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ContactData>().ToTable("Contacts");
+    }
+}
+
+public class ContactData
+{
+    [Key, Required]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Family { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
+    [Phone]
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string Description { get; set; }
+}
---
+++ ContactApp/Migrations/20220120162636_Init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactApp.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Family = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
